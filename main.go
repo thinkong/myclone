42249dc56d590e0cc7256e46dfb09c9462c97968
Kyuhun Lee <thinkong@hotmail.com>
2017-04-13 22:45:05 +0900

    changed so it would look like a library.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main;
+package myclone;
 
 import (
 	"github.com/gin-gonic/gin"
@@ -7,8 +7,10 @@ import (
 	"sync/atomic"
 	"sync"
 	"github.com/satori/go.uuid"
+	. "github.com/ahmetb/go-linq"
 )
 
+// Entry
 type Entry struct {
 	Title    string
 	Contents string
@@ -16,10 +18,14 @@ type Entry struct {
 	Downvote int
 	Uid      string
 }
+// DataStore.
+// atomic.Value makes sure that it would be atomic.
 var dataStore atomic.Value
 var lock sync.Mutex
+
 type DBMap map[string]*Entry
 
+// InsertValue
 func InsertValue(newEntry *Entry){
 	lock.Lock()
 	defer lock.Unlock()
@@ -31,7 +37,7 @@ func InsertValue(newEntry *Entry){
 	db2[newEntry.Uid] = newEntry
 	dataStore.Store(db2)
 }
-
+//downvote
 func Downvote(uid string){
 	lock.Lock()
 	defer lock.Unlock()
@@ -43,7 +49,7 @@ func Downvote(uid string){
 	db2[uid].Downvote++
 	dataStore.Store(db2)
 }
-
+//upvote
 func Upvote(uid string){
 	lock.Lock()
 	defer lock.Unlock()
@@ -56,21 +62,36 @@ func Upvote(uid string){
 	dataStore.Store(db2)
 }
 
-func main() {
+var r = gin.Default();
+func init() {
 	dataStore.Store(make(DBMap))
 
-	r := gin.Default();
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", indexpage)
-
 	r.POST("/write", WritePost)
+	r.GET("/up/:title", func(c *gin.Context){
+		uid := c.Param("title")
+		Upvote(uid)
+		c.Redirect(http.StatusTemporaryRedirect, "/")
+	})
+	r.GET("/down/:title", func(c *gin.Context){
+		uid := c.Param("title")
+		Downvote(uid)
+		c.Redirect(http.StatusTemporaryRedirect, "/")
+	})
 
+}
+
+func Start(){
 	log.Fatalln(r.Run(":8080"))
 }
 
 func WritePost (c *gin.Context) {
 	title := c.PostForm("title")
 	contents := c.PostForm("contents")
+	// make sure there is data in contents and title..
+	// this should be in the tmpl file
+	// lazyness to implement it in the tmpl file
 	if contents == ""{
 		c.Redirect(http.StatusMovedPermanently, "/")
 	}
@@ -87,7 +108,15 @@ func WritePost (c *gin.Context) {
 }
 
 func indexpage(c *gin.Context) {
-	items := dataStore.Load().(DBMap)
+	var items []*Entry
+
+	db1 := dataStore.Load().(DBMap)
+	From(db1).Select(func(e interface{})interface{}{
+		return e.(KeyValue).Value
+	}).ToSlice(&items)
+	From(items).OrderByDescending(func(e interface{}) interface{}{
+		return e.(*Entry).Upvote
+	}).Take(20).ToSlice(&items)
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"Items": items,
 	})
---
+++ main/main.go
@@ -0,0 +1,5 @@
+package main
+import "github.com/thinkong/myclone"
+func main(){
+	myclone.Start()
+}
